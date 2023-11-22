e4dde7289478280e57b7b590b49e850478c4979c
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2023-11-22 18:13:16 +0800

    lam mo sir, unti nalang give up na ako, sa life

--- src/components/Receiver.jsx
+++ src/components/Receiver.jsx
@@ -1,12 +1,20 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin, googleLogout} from '@react-oauth/google';
 
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 const Receiver = () => {
+
+  const login = useGoogleLogin({
+    onSuccess: tokenResponse => console.log(tokenResponse),
+    flow: 'auth-code',
+  });
+
   return (
     <div
       className={`xl:mt-12 overflow-hidden`}
@@ -17,9 +25,15 @@ const Receiver = () => {
       >
         <p className={styles.sectionSubText}>Admin Dashboard</p>
         <h3 className={styles.sectionHeadText}>Received Messages</h3>
-        <div className="rounded-2xl w-full">
-          <GoogleLogin onSuccess={credentialResponse => {console.log(credentialResponse);}} onError={() => {console.log('Login Failed');}} />
+        <div className='flex flex-row justify-between items-center mt-5 gap-5'>
+          <button onClick={() => login()} className="w-1/2 text-xl rounded-2xl text-tertiary outline-none bg-white font-bold shadow-md shadow-primary py-5 mx-auto " >
+            <FontAwesomeIcon icon={faGoogle} className='text-2xl mr-1' /> Sign in with Google
+          </button>
+          <button onClick={() => googleLogout()} className="w-1/2 text-xl rounded-2xl text-tertiary outline-none bg-white font-bold shadow-md shadow-primary py-5 mx-auto" >
+            Logout
+          </button>
         </div>
+
         
       </motion.div>
     </div>
