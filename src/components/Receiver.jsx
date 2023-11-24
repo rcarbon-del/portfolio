b88043b24bb820e7e19e024c730e2dbaea7f1eeb
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2023-11-24 13:44:21 +0800

    haha yoko na

--- src/components/Receiver.jsx
+++ src/components/Receiver.jsx
@@ -8,55 +8,16 @@ import { slideIn } from "../utils/motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
-import { useFetch } from "../hooks";
-
-const FetchGmailWrapper = () => {
-  const [messages, setMessages] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const { data, error: fetchError, loading: fetchLoading } = useFetch(
-    "https://www.googleapis.com/gmail/v1/users/me/messages?maxResults=10&labelIds=INBOX&key=AIzaSyD-1z8K6X7fWzKl1R4U2s4Dp0L0QZQfjVw"
-  );
-
-  return (
-    <FetchGmail
-      messages={messages}
-      setMessages={setMessages}
-      loading={loading}
-      setLoading={setLoading}
-      error={error}
-      setError={setError}
-      data={data}
-      fetchError={fetchError}
-      fetchLoading={fetchLoading}
-    />
-  );
-}
 
 const Receiver = () => {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(() => {
-    const saved = localStorage.getItem('isLoggedIn');
-    const initialValue = JSON.parse(saved);
-    return initialValue || false;
-  });
-
-  useEffect(() => {
-    localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
-  }, [isLoggedIn]);
-
   const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      console.log(tokenResponse.accessToken);
-      if (tokenResponse.accessToken) {
-        setIsLoggedIn(true);
-      }
+    onSuccess: (response) => {
+      console.log(response.accessToken);
+      // You can now use the access token
     },
     onFailure: (error) => console.log(error),
   });
-
-  const logout = googleLogout();
 
   return (
     <>
@@ -69,16 +30,13 @@ const Receiver = () => {
         >
           <p className={styles.sectionSubText}>Admin Dashboard</p>
           <h3 className={styles.sectionHeadText}>Received Messages</h3>
-          <div className='mt-5'>
-          {isLoggedIn ? (
-            <button onClick={() => {logout(); setIsLoggedIn(false);}} className="w-full text-xl rounded-2xl text-tertiary outline-none bg-white font-bold shadow-md shadow-primary py-5 mx-auto">
-              Logout
-            </button>
-          ) : (
-            <button onClick={() => login()} className="w-full text-xl rounded-2xl text-tertiary outline-none bg-white font-bold shadow-md shadow-primary py-5 mx-auto">
+          <div className='flex flex-row justify-between items-center mt-5 gap-5'>
+            <button onClick={() => login()} className="w-1/2 text-xl rounded-2xl text-tertiary outline-none bg-white font-bold shadow-md shadow-primary py-5 mx-auto">
               <FontAwesomeIcon icon={faGoogle} className='text-2xl mr-1' /> Sign in with Google
             </button>
-          )}
+            <button onClick={googleLogout()} className="w-1/2 text-xl rounded-2xl text-tertiary outline-none bg-white font-bold shadow-md shadow-primary py-5 mx-auto">
+              Logout
+            </button>
           </div>
         </motion.div>
       </div>
