9f52d467db4f93d48354c71fe7205bf63a5853e6
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2024-01-28 09:33:33 +0800

    rar

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ const App = () => {
             <Contact />
             <StarsCanvas />
           </div>
-          <Receiver />
         </div>
       </BrowserRouter>
     </GoogleOAuthProvider>
